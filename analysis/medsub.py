8a7823789a722d37c6704043030689d7a4571707
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2025-08-15 07:06:14 -0400

    moment stuff

--- analysis/medsub.py
+++ analysis/medsub.py
@@ -3,15 +3,43 @@ import os
 basepath = '/orange/adamginsburg/w51/2017.1.00293.S'
 
 fns = {(sourcename, spw): f'{basepath}/line_cubes/W51{sourcename}_only.B3.robust0.5.spw{spw}.clarkclean1e5.1536.bigpix.image.pbcor.fits'
-       for sourcename in ('w51e5', 'w51e_n', 'w51n', 'w51main', 'w51e8', 'w51sw', 'w51n_mm24')
+       for sourcename in ('w51n', 'w51e5', 'w51e_n', 'w51main', 'w51e8', 'w51sw', 'w51n_mm24', 'w51noutflow')
        for spw in (0, 1, 2, 3)}
 
 
+from statcont.cont_finding import c_sigmaclip_scube
+from astropy.io import fits
 for fn in fns.values():
+    outfn = fn.replace('.fits', '.cont.fits')
+    if os.path.exists(outfn):
+        cont = fits.getdata(outfn)
+    else:
+        print(f"Finding continuum for {fn}")
+        cube = SpectralCube.read(fn, use_dask=True)
+        noise = cube.std()
+        result = c_sigmaclip_scube(cube, noise,
+                                    verbose=True,
+                                    save_to_tmp_dir=True)
+        data_to_write = result[1].compute()
+        cont = data_to_write.value
+
+        print(f"Writing to FITS {outfn}", flush=True)
+        fits.PrimaryHDU(data=cont,
+                        header=cube[0].header).writeto(outfn, overwrite=True)
+
+    outfn = fn.replace('.fits', '.contsub.fits')
+    if not os.path.exists(outfn):
+        print(f"Subtracting continuum for {fn}")
+        cube = SpectralCube.read(fn, use_dask=True)
+        contsub = cube - cont
+        contsub.write(outfn)
+
+
     cube = SpectralCube.read(fn)
     if not os.path.exists(fn.replace('.fits', '.medsub.fits')):
         print(f"Median subtracting {fn}")
         cube.beam_threshold = 0.1
+        cube.allow_huge_operations = True
         med = cube.median(axis=0)
         medsub = cube - med
         medsub.write(fn.replace('.fits', '.medsub.fits'))
--- analysis/moments.py
+++ analysis/moments.py
@@ -4,3 +4,23 @@ basepath = '/orange/adamginsburg/w51/2017.1.00293.S'
 fns = {sourcename: f'{basepath}/line_cubes/W51{sourcename}_only.B3.robust0.5.spw{spw}.clarkclean1e5.1536.bigpix.image.pbcor.medsub.fits'
        for sourcename in ('w51e5', 'w51e_n', 'w51n', 'w51main', 'w51e8', 'w51sw', 'w51n_mm24')
        for spw in (0, 1, 2, 3)}
+
+lines = {'SiO2-1_v=0':  86846.96*u.MHz,
+         'SiO2-1_v=1': 86243.4277*u.MHz,
+         'SiO2-1_v=2': 85640.453*u.MHz,
+         '29SiO2-1_v=0': 85759.194*u.MHz,
+         'HCN1-0_v=0': 88630.4157*u.MHz,
+         'CS2-1_v=0': 97980.953*u.MHz,
+         'SO32-21': 99299.87*u.MHz,
+}
+
+vrange_blue = (30, 51)*u.km/u.s
+vrange_red = (69, 95)*u.km/u.s
+
+for sourcename, fn in fns.items():
+    cube = SpectralCube.read(fn)
+    cube.beam_threshold = 0.1
+    for line, freq in lines.items():
+        vcube = cube.with_spectral_unit(u.km/u.s, velocity_convention='radio', rest_value=freq)
+        vcube.spectral_slab(*vrange_blue).moment0(axis=0).write(f'{basepath}/moments/{sourcename}_{line}_v=0_blue.fits')
+        vcube.spectral_slab(*vrange_red).moment0(axis=0).write(f'{basepath}/moments/{sourcename}_{line}_v=0_red.fits')
